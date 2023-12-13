f561b159aef77fbd7d68233a17c0e6652477831f
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-13 10:57:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("6876018655"))
+OWNER_ID = int(getenv("OWNER_ID", "6876018655"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
