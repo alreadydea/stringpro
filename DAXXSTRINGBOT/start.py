a8781643c46abe4ba1ac2e84fe6711c68cd12b31
Gagan <119489004+devgaganin@users.noreply.github.com>
2023-12-13 10:20:50 +0530

    Update start.py

--- DAXXSTRINGBOT/start.py
+++ DAXXSTRINGBOT/start.py
@@ -19,15 +19,15 @@ async def start(bot: Client, msg: Message):
 𝐅𝐔𝐋𝐋𝐘 𝐒𝐀𝐅𝐄 & 𝐒𝐄𝐂𝐔𝐑𝐄.
 𝐍𝐎  𝐄𝐑𝐑𝐎𝐑.
 
-𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝐓𝐄𝐀𝐌 𝐃𝐀𝐗𝐗](tg://user?id={OWNER_ID}) !""",
+𝐌𝐚𝐝𝐞 𝐁𝐲  : [TEAM SPY](tg://user?id={OWNER_ID}) !""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="🌿𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆🌿", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton(" 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/AllPremiumCc"),
-                    InlineKeyboardButton("𝐎𝐟𝐟𝐢𝐜𝐞", url="https://t.me/CYBERDAXXX")
+                    InlineKeyboardButton(" Channel𝐭", url="https://t.me/dev_gagan"),
+                    InlineKeyboardButton("Group", url="https://t.me/dev_gagan")
                 ]
             ]
         ),
